6af502950ec3c07f0974fd3526a9c68e1d13dfd0
dlamoureux <lamoureux.d@northeastern.edu>
2022-04-03 07:55:20 -0400

    Log out on account activity added

--- app/src/main/java/edu/neu/madcourse/charge/AccountActivity.java
+++ app/src/main/java/edu/neu/madcourse/charge/AccountActivity.java
@@ -4,26 +4,30 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.widget.Button;
 
-public class LandingPageActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class AccountActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
 
         // Set the custom app bar view
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.toolbar_custom);
 
-        setContentView(R.layout.activity_landing_page);
+        auth = FirebaseAuth.getInstance();
 
-        ImageView account = findViewById(R.id.accountImageView);
+        Button logoutButton = findViewById(R.id.buttonLogout);
 
-        account.setOnClickListener(view -> {
-            startActivity(new Intent(LandingPageActivity.this, AccountActivity.class));
+        logoutButton.setOnClickListener(view -> {
+            auth.signOut();
+            startActivity(new Intent(AccountActivity.this, LoginActivity.class));
         });
     }
 }
