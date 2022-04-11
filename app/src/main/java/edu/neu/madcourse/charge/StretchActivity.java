998eac5a2e9c825265c18301541226f9f38ec52d
jbellegarde <bellegarde.j@northeastern.edu>
2022-04-11 11:34:30 -0400

    Added video recycler view and layout view in resource folder

--- app/src/main/java/edu/neu/madcourse/charge/StretchActivity.java
+++ app/src/main/java/edu/neu/madcourse/charge/StretchActivity.java
@@ -9,7 +9,7 @@ public class StretchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stretch);
+        setContentView(R.layout.activity_stretch_recyclerview);
     }
 
     // RecyclerView of preloaded videos
