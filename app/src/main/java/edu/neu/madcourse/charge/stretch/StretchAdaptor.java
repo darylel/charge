5dd178fe69112ba119c30c55263e242c2f72356c
jbellegarde <bellegarde.j@northeastern.edu>
2022-04-15 15:27:24 -0400

    new branch work

---
+++ app/src/main/java/edu/neu/madcourse/charge/stretch/StretchAdaptor.java
@@ -0,0 +1,48 @@
+package edu.neu.madcourse.charge.stretch;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.neu.madcourse.charge.R;
+
+public class StretchAdaptor extends RecyclerView.Adapter<StretchHolder> {
+
+    private List<StretchVideo> stretchVideoVideoList;
+    private StretchClickListener stretchClickListener;
+
+    public StretchAdaptor() {}
+
+    public StretchAdaptor(List<StretchVideo> stretchVideoVideoList) {
+        this.stretchVideoVideoList = stretchVideoVideoList;
+    }
+
+
+    public void setStretchClickListener(StretchClickListener stretchClickListener) {
+        this.stretchClickListener = stretchClickListener;
+    }
+
+    @NonNull
+    @Override
+    public StretchHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.stretch_layout, parent, false);
+        return new StretchHolder(view, stretchClickListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StretchHolder holder, int position) {
+        StretchVideo currentStretchVideo = stretchVideoVideoList.get(position);
+        holder.stretchWebView.loadData(currentStretchVideo.getUrl(), "text/html", "utf-8");
+    }
+
+    @Override
+    public int getItemCount() {
+        return stretchVideoVideoList.size();
+    }
+}
