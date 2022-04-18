59b0e11cbb9b759a0b4b6ccb05c982672994274b
jbellegarde <bellegarde.j@northeastern.edu>
2022-04-18 12:42:38 -0400

    Working on adapter/holder

--- app/src/main/java/edu/neu/madcourse/charge/stretch/StretchHolder.java
+++ app/src/main/java/edu/neu/madcourse/charge/stretch/StretchHolder.java
@@ -3,20 +3,26 @@ package edu.neu.madcourse.charge.stretch;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import edu.neu.madcourse.charge.R;
 
 public class StretchHolder extends RecyclerView.ViewHolder {
-    protected WebView stretchWebView;
+//    protected WebView stretchWebView;
+    protected TextView textView;
+    protected ImageView imageView;
 
     public StretchHolder(View itemView, final StretchClickListener stretchClickListener) {
         super(itemView);
-        stretchWebView = itemView.findViewById(R.id.stretch_imageview);
-//        itemView.setOnClickListener(v -> stretchClickListener.onStretchClick(getLayoutPosition()));
-        stretchWebView.getSettings().setJavaScriptEnabled(true);
-        stretchWebView.setWebChromeClient(new WebChromeClient());
+        imageView = itemView.findViewById(R.id.stretch_imageview);
+        textView = itemView.findViewById(R.id.stretch_textview);
+//        stretchWebView = itemView.findViewById(R.id.stretch_imageview);
+////        itemView.setOnClickListener(v -> stretchClickListener.onStretchClick(getLayoutPosition()));
+//        stretchWebView.getSettings().setJavaScriptEnabled(true);
+//        stretchWebView.setWebChromeClient(new WebChromeClient());
     }
 
 }
