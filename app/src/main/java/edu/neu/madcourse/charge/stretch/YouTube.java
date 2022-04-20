41340294ad8434f07c2253ec8b80827e95e86d84
jbellegarde <bellegarde.j@northeastern.edu>
2022-04-20 00:12:19 -0400

    Finishing up tomorrow

--- app/src/main/java/edu/neu/madcourse/charge/stretch/YouTube.java
+++ app/src/main/java/edu/neu/madcourse/charge/stretch/YouTube.java
@@ -2,15 +2,27 @@ package edu.neu.madcourse.charge.stretch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 import edu.neu.madcourse.charge.R;
 
 public class YouTube extends AppCompatActivity {
+    YouTubePlayerView youTubePlayerView;
+    Intent videoIntent;
+    String video;
+    String videoId = "videoId";
+    String API_KEY = "AIzaSyCgyiPkLYH0jQI0wrZlhzZscbMCPnJmSt4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_you_tube);
+        youTubePlayerView = findViewById(R.id.youtube_player);
+        videoIntent = getIntent();
+        video = videoIntent.getStringExtra(videoId);
     }
 }
