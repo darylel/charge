5d97bfcbc460c69e1ec09a31d406104cf9ffaeca
jbellegarde <bellegarde.j@northeastern.edu>
2022-04-20 15:37:02 -0400

    Applied video

--- app/src/main/java/edu/neu/madcourse/charge/stretch/YouTube.java
+++ app/src/main/java/edu/neu/madcourse/charge/stretch/YouTube.java
@@ -1,21 +1,22 @@
 package edu.neu.madcourse.charge.stretch;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
 import edu.neu.madcourse.charge.R;
 
-public class YouTube extends AppCompatActivity {
+public class YouTube extends YouTubeBaseActivity implements YouTubePlayer.PlaybackEventListener,
+        YouTubePlayer.OnInitializedListener, YouTubePlayer.PlayerStateChangeListener {
     YouTubePlayerView youTubePlayerView;
     Intent videoIntent;
-    String video;
     String videoId = "videoId";
-    String API_KEY = "AIzaSyCgyiPkLYH0jQI0wrZlhzZscbMCPnJmSt4";
+    String API_KEY = "AIzaSyCgyiPkLYH0jQI0wrZlhzZscbMCPnJmSt4"; // will need store and retrieve  from firebase
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,77 @@ public class YouTube extends AppCompatActivity {
         setContentView(R.layout.activity_you_tube);
         youTubePlayerView = findViewById(R.id.youtube_player);
         videoIntent = getIntent();
-        video = videoIntent.getStringExtra(videoId);
+        youTubePlayerView.initialize(API_KEY, this);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        youTubePlayer.setPlayerStateChangeListener(this);
+        youTubePlayer.setPlaybackEventListener(this);
+
+        if (b) {
+            return;
+        }
+        youTubePlayer.cueVideo(videoIntent.getStringExtra(videoId));
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
+
+    @Override
+    public void onPlaying() {
+
+    }
+
+    @Override
+    public void onPaused() {
+
+    }
+
+    @Override
+    public void onStopped() {
+
+    }
+
+    @Override
+    public void onBuffering(boolean b) {
+
+    }
+
+    @Override
+    public void onSeekTo(int i) {
+
+    }
+
+    @Override
+    public void onLoading() {
+
+    }
+
+    @Override
+    public void onLoaded(String s) {
+
+    }
+
+    @Override
+    public void onAdStarted() {
+
+    }
+
+    @Override
+    public void onVideoStarted() {
+
+    }
+
+    @Override
+    public void onVideoEnded() {
+
+    }
+
+    @Override
+    public void onError(YouTubePlayer.ErrorReason errorReason) {
+        Toast.makeText(this, errorReason.toString(), Toast.LENGTH_SHORT).show();
     }
 }
