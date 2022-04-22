23a709fcd9ec70fb18d579ec8df8a0700b6a75cc
bmesfin <mesfin.bethlehem@gmail.com>
2022-04-22 13:57:39 -0400

    Journal class updated with unique ID attribute

--- app/src/main/java/edu/neu/madcourse/charge/Journal.java
+++ app/src/main/java/edu/neu/madcourse/charge/Journal.java
@@ -6,13 +6,14 @@ package edu.neu.madcourse.charge;
 public class Journal {
     private String journalTitle;
     private String journalDate;
-    //TODO: add the unique ID
+    private String journalID;
 
     public Journal() {}
 
-    public Journal(String journalTitle, String journalDate) {
+    public Journal(String journalTitle, String journalDate, String journalID) {
         this.journalTitle = journalTitle;
         this.journalDate = journalDate;
+        this.journalID = journalID;
     }
 
     public String getJournalTitle() {
@@ -26,4 +27,8 @@ public class Journal {
     public String getJournalDate() { return journalDate; }
 
     public void setJournalDate(String journalDate) { this.journalDate = journalDate; }
+
+    public String getJournalID() { return journalID; }
+
+    public void setJournalID(String journalID) { this.journalID = journalID; }
 }
